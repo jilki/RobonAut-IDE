26ba1d7b7b11f385961a523641732b96449b5d92
Thamó Zsolt <thamozsolt@hotmail.com>
2017-12-12 20:25:05 +0100

    filters added

--- spi_tester.ino
+++ spi_tester.ino
@@ -26,6 +26,7 @@
 #include <SPI.h>
 #include <Servo.h>
 #include <math.h>
+#include <elapsedMillis.h>
 #define _servopin  32
 #define _motorpin  6     
 Servo servoMotor;
@@ -38,7 +39,9 @@ int baseservo=1550;
 int basemotor=1500;
 int upper=1610;
 float linePosold=0;
+float oldPos = 0;
 
+elapsedMillis timeElapsed;
 
 //Sensor's memory register addresses:
 const int PRESSURE = 0x1F;      //3 most significant bits of pressure
@@ -46,7 +49,7 @@ const int PRESSURE_LSB = 0x20;  //16 least significant bits of pressure
 const int TEMPERATURE = 0x21;   //16 bit temperature reading
 const byte READ = 0b11111100;     // SCP1000's read command
 const byte WRITE = 0b00000010;   // SCP1000's write command
-const int delay_us = 100;
+const int delay_us = 15;
 
 // pins used for the connection with the sensor
 // the other you need are controlled by the SPI library):
@@ -95,73 +98,140 @@ void loop() {
   byte pos[4][8] = {{0}};
   byte ans[4][8] = {{0}};
 
-  readSensor(51, pos[0], ans[0]);
+  //readSensor(51, pos[0], ans[0]);
 /*
   digitalWrite(44,HIGH);
   delay(1);
   digitalWrite(44,LOW);
   delay(5);
   */
-  readSensor(10, pos[1], ans[1]);
+  //readSensor(10, pos[1], ans[1]);
 
 /*  digitalWrite(44,HIGH);
   delay(1);
   digitalWrite(44,LOW);
   delay(5);
   */
-  readSensor(5,  pos[2], ans[2]);
+  //readSensor(5,  pos[2], ans[2]);
 
 /*  digitalWrite(44,HIGH);
   delay(1);
   digitalWrite(44,LOW);
   delay(5);
   */
-  readSensor(4,  pos[3], ans[3]);
-  
+  //readSensor(4,  pos[3], ans[3]);
 
+  byte muliDimAns[3][4][8];
+  readSensor(51, pos[0], muliDimAns[0][0]);
+  readSensor(10, pos[0], muliDimAns[0][1]);
+  readSensor(5,  pos[0], muliDimAns[0][2]);
+  readSensor(4,  pos[0], muliDimAns[0][3]);
+  readSensor(51, pos[1], muliDimAns[1][0]);
+  readSensor(10, pos[1], muliDimAns[1][1]);
+  readSensor(5,  pos[1], muliDimAns[1][2]);
+  readSensor(4,  pos[1], muliDimAns[1][3]);
+  readSensor(51, pos[2], muliDimAns[2][0]);
+  readSensor(10, pos[2], muliDimAns[2][1]);
+  readSensor(5,  pos[2], muliDimAns[2][2]);
+  readSensor(4,  pos[2], muliDimAns[2][3]);
 
-  //printSensorValue(51,  ans[0], pos[0]);
-  //printSensorValue(10,  ans[1], pos[1]);
-  //printSensorValue(5, ans[2], pos[2]);
-  //printSensorValue(4, ans[3], pos[3]);
+  for(int i = 0; i<4; i++){
+    for(int j=0; j<8; j++){
+      if(muliDimAns[0][i][j] == muliDimAns[1][i][j]){
+        ans[i][j] = muliDimAns[0][i][j];
+      }else if(muliDimAns[1][i][j] == muliDimAns[2][i][j]){
+        ans[i][j] = muliDimAns[1][i][j];
+      }else if(muliDimAns[0][i][j] == muliDimAns[2][i][j]){
+        ans[i][j] = muliDimAns[0][i][j];       
+      }else{
+        ans[i][j] = muliDimAns[0][i][j]*0.33 + muliDimAns[1][i][j]*0.33 + muliDimAns[2][i][j]*0.33;        
+      }
+    }
+  }
+
+//  printSensorValue(51,  ans[0], pos[0]);
+//  printSensorValue(10,  ans[1], pos[1]);
+//  printSensorValue(5, ans[2], pos[2]);
+//  printSensorValue(4, ans[3], pos[3]);
 
   int num = 0;
   int denum = 0;
-  for(int i = 0; i < 4; i++){
+  
+  /*for(int i = 0; i < 4; i++){
     for(int j = 0; j < 8; j++){
       if(ans[i][j]>160){
         num += ((i*8)+j-16)*ans[i][j];
         denum += ans[i][j];
       }
     }
+  }*/
+
+  int linValues[32];
+   for(int j = 0; j < 4; j++){
+    for(int k = 0; k < 8; k++){
+      linValues[j*8+k] = ans[j][k];
+      }
+    }
+   
+/*  Serial.println("\nLinearised");
+  for(int i=0; i<32; i++){
+    Serial.print(linValues[i]);
+    Serial.print(" ");
+  }*/
+
+//  Serial.println("linearization finished");
+  int filtered[28];
+ 
+  for(int i=0; i < 28;i++){
+    filtered[i] = linValues[i]*0.11 + linValues[i+1]*0.22 + linValues[i+2]*0.34 + linValues[i+3]*0.22 + linValues[i+4]*0.11;
+       if(filtered[i]>70){
+        num += (i-14)*filtered[i];
+        denum += filtered[i];
+      }
   }
 
+//  Serial.println("\nFiltered");
+//  for(int i=0; i<28; i++){
+//    Serial.print(filtered[i]);
+//    Serial.print(" ");
+//  }
   
   //Serial.print("\n\n\n\nCalcuated line pos: ");
-  //Serial.println(num/(float)denum);
+  Serial.println(num/(float)denum+0.5);
+  //Serial.println(timeElapsed);
   
   float linePos=num/(float)denum+0.5;
   
-  //Serial.print("\n\n\LinePos in float");
+/*  Serial.println("\n\n");
   Serial.println(linePos);
   Serial.print("deltahiba:");
   Serial.println(linePos-linePosold);
   Serial.print("ratioD:");
   Serial.println(ratioD);
-  
-  //if(linePos<-0.01 || linePos>0.01)
+  Serial.print("Lineposold:");
+  Serial.println(linePosold);
+
+  Serial.print("P*hiba:");
+  Serial.println(ratioP*linePos);
+  Serial.print("D*deltahiba");
+  Serial.println(ratioD*(linePos-linePosold));
+*/
+
   if(!isnan(linePos))
   {
-    if(linePos>5 || linePos < -5){
-       motorM.writeMicroseconds(upper);
-       servoMotor.writeMicroseconds(baseservo+ratioP*linePos+ratioD*(linePos-linePosold));
-       //Serial.print("\n\nbeléptem a felételebe");
-     }
-     else{
-       motorM.writeMicroseconds(upper);
-       servoMotor.writeMicroseconds(baseservo+ratioP*linePos+ratioD*(linePos-linePosold));
-     }
-     linePosold=linePos;
+    float diff = linePos-linePosold;
+    if(diff < 2 && diff > -2){
+      if(linePos>5 || linePos < -5){
+         motorM.writeMicroseconds(upper);
+         servoMotor.writeMicroseconds(baseservo+ratioP*linePos+ratioD*(linePos-linePosold));
+         //Serial.print("\n\nbeléptem a felételebe");
+       }
+       else{
+         motorM.writeMicroseconds(upper);
+         servoMotor.writeMicroseconds(baseservo+ratioP*linePos+ratioD*(linePos-linePosold));
+       }
+    }
+    linePosold=linePos;
   }
   else{
       if(linePosold<-12){
@@ -173,12 +243,6 @@ void loop() {
        //servoMotor.writeMicroseconds(base);
        motorM.writeMicroseconds(basemotor);
       }
-   Serial.print("P*hiba:");
-   Serial.println(ratioP*linePos);
-   Serial.print("D*deltahiba");
-   Serial.println(ratioD*(linePos-linePosold));
-   Serial.print("Lineposold:");
-   Serial.println(linePosold);
 
    
 }
@@ -253,7 +317,7 @@ void readSensor(int boardNum, byte *pos, byte *ans){
       ans[4]=ans[3];
   }
 
-  if(ans[5]==198){
+  if(ans[5]==198 || ans[5]==199){
     digitalWrite(boardNum, LOW);
     delayMicroseconds(delay_us);
   
@@ -264,7 +328,7 @@ void readSensor(int boardNum, byte *pos, byte *ans){
     delayMicroseconds(delay_us);  
   }
   
-  if(ans[5]==198){
+  if(ans[5]==198 || ans[5]==199){
     digitalWrite(boardNum, LOW);
     delayMicroseconds(delay_us);
   
@@ -275,8 +339,8 @@ void readSensor(int boardNum, byte *pos, byte *ans){
     delayMicroseconds(delay_us);  
   }
 
-  if(ans[5]==198){
-    if(ans[5]!=199)
+  if(ans[5]==198 || ans[5]==199){
+    if(ans[4]!=199)
       ans[5]=(ans[4]+ans[6])/2;
     else
       ans[5]=ans[6];
